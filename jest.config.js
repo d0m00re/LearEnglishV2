4ea47ff864a7785ee2ae786cbc1683dfa3253fee
jacklapiquette <jackLapiquette@oss.117>
2021-05-15 23:10:33 +0200

    (+) add moore tool husky + lint-staged

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+
+const base = require('./jest.config.base');
+
+module.exports = {
+    ...base,
+    roots: ['<rootDir>'],
+    projects: [
+      '<rootDir>/packages/ui',
+      '<rootDir>/packages/api',
+      '<rootDir>/packages/diceroll'
+    ],
+};
